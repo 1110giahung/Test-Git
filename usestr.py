0d52d59b2bf24b0e895991c2c9a22f9c0b92d3ee
1110giahung <144457830+1110giahung@users.noreply.github.com>
2024-07-03 21:12:30 +0700

    Create usestr.py

---
+++ usestr.py
@@ -0,0 +1,19 @@
+'''
+day la chu thich
+'''
+str =   '\a\a hung \b minh \" \t'
+print(str)
+
+tran = r'toi la \nguoi'
+print(tran[2:None:-2])
+
+strA = float("6.4")
+print(strA)
+
+strB = "HowToWin"
+strB = strB[None:1] + "0" + strB[2:]
+print(strB)
+
+s = "abc xyz"
+print(s[len(s):])
+
