29a906da63609b225a94a9f810c744b1e98c5f4f
1110giahung <144457830+1110giahung@users.noreply.github.com>
2024-07-07 19:32:56 +0700

    Update usestr.py

--- usestr.py
+++ usestr.py
@@ -17,3 +17,38 @@ print(strB)
 s = "abc xyz"
 print(s[len(s):])
 
+#string
+print('hung is %s' % ('minh'))
+s = '%s %s'
+result1 = s%('1','2')
+print(result1)
+
+#repr
+
+
+#float
+f = '%f' %(2.4124312)
+f2 = '%.2f' %(2.4124312)
+print(f + '  ' + f2)
+
+# f { abc }  : format
+name = 'Hung'
+age = '20'
+address = 'BD'
+info = f'name: {name}\nage: {age}\naddress: {address}'
+print(info)
+
+#format
+print('a{}b{}c{}'.format(1,2,3))
+print('a={1}b={0}c={2}'.format('zero','one','three'))
+print('a={num1}, b={num2}'.format(num1=22, num2=33))
+
+#allignment
+#{:<10} left  {*:<10}
+#{:>10} right
+#{:^10} center
+print('{:^10}'.format('a'))
+print('{:*^10}'.format('a'))
+print('{:>10}'.format('a'))
+
+
